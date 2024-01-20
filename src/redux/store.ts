b6787f5b866e9910b5684b4807b4dab8a55be802
DalalBassam9 <dalalbassam2@gmail.com>
2024-01-20 23:13:31 +0300

    add

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { cartReducer } from "./cartSlice";
-import cartMiddleware from './cartMiddleware';
-import {  Middleware } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import wishlistReducer from "./wishlistSlice";
-import { AnyAction, Dispatch } from 'redux';
 
 export const store = configureStore({
    reducer: {
