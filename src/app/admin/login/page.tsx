4737487a0764f75a8b52bc8e56da10d8657f4b88
DalalBassam9 <dalalbassam2@gmail.com>
2024-01-25 20:30:25 +0300

    add

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -30,14 +30,6 @@ export default function Login() {
         password: "",
         email: ""
     });
-  
-    useEffect(() => {
-        // Perform localStorage action
-        if (typeof window !== 'undefined') {
-            const token = localStorage.getItem('token');
-            // Use the token...
-        }
-    }, []);
 
 
     const validateField = async (field: string, value: string) => {
@@ -62,150 +54,156 @@ export default function Login() {
 
         validateField(field, value);
     };
+    useEffect(() => {
+        const handleSubmit = async (e: React.FormEvent) => {
+            e.preventDefault();
+            try {
+                setLoading(true);
+                await schema.validate(formData, { abortEarly: false });
+                const response = axios.post(process.env.NEXT_PUBLIC_API_URL + `/api/login`, formData);
+                const token = (await response).data.access_token;
+                
+                if (typeof window !== 'undefined' && window.localStorage) {
+                    localStorage.setItem("token", token);
+                }
 
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
-        try {
-            setLoading(true);
-            await schema.validate(formData, { abortEarly: false });
-            const response = axios.post(process.env.NEXT_PUBLIC_API_URL + `/api/login`, formData,
-
-            );
-            const token = (await response).data.access_token
-     
-
-            Swal.fire({
-                icon: 'success',
-                title: 'Success',
-                text: ' login  successfully',
-                confirmButtonColor: '#D5A983'
-            })
-
-            router.push("/admin");
-        } catch (error: any) {
-            if (error instanceof Yup.ValidationError) {
-                const errors: { [key: string]: string } = {}; // Specify the type of 'errors' object
-                error.inner.forEach((error: any) => {
-                    errors[error.path] = error.message;
-                });
-                setErrors(errors);
-            } else {
                 Swal.fire({
-                    icon: 'error',
-                    title: 'Oops...',
-                    text: error.response?.data?.message || error.message,
+                    icon: 'success',
+                    title: 'Success',
+                    text: ' login successfully',
                     confirmButtonColor: '#D5A983'
                 });
 
+                router.push("/admin");
+            } catch (error: any) {
+                if (error instanceof Yup.ValidationError) {
+                    const errors: { [key: string]: string } = {};
+                    error.inner.forEach((error: any) => {
+                        errors[error.path] = error.message;
+                    });
+                    setErrors(errors);
+                } else {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: error.response?.data?.message || error.message,
+                        confirmButtonColor: '#D5A983'
+                    });
+                }
+            } finally {
+                setLoading(false);
             }
-        } finally {
-            setLoading(false);
-        }
+        };
 
-    };
+        // Return cleanup function to avoid memory leaks
+        return () => {};
+    }, []); // Empty dependency array ensures the effect runs only once on component mount
 
+    // Your component JSX code here
+};
+  
     return (
-            <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
 
 
-                <ToastContainer />
-                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+            <ToastContainer />
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
 
-                    <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
+                <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
 
-                        <div className="sm:mx-auto my-6 sm:w-full sm:max-w-md">
+                    <div className="sm:mx-auto my-6 sm:w-full sm:max-w-md">
 
-                            <Link href="/" passHref>
-                                <h2 className="text-3xl  text-center font-bold tracking-tight  text-beige">Roshan Books</h2>
-                            </Link>
+                        <Link href="/" passHref>
+                            <h2 className="text-3xl  text-center font-bold tracking-tight  text-beige">Roshan Books</h2>
+                        </Link>
 
-                            <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                                Sign in to your account
-                            </h2>
-                        </div>
-
-
-
-                        <form className="space-y-6" noValidate autoComplete="off" onSubmit={handleSubmit} >
-                            <div>
-                                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                                    Email address
-                                </label>
-                                <div className="mt-2">
-                                    <input
-                                        id="email"
-                                        name="email"
-                                        type="email"
-                                        value={formData.email}
-                                        onChange={(e) => handleChange('email', e.target.value)}
-                                        autoComplete="email"
-                                        required
-                                        className="form-input block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-beige sm:text-sm sm:leading-6"
-                                    />
-                                    <div className="text-red-500 text-sm mt-2">{errors.email}</div>
-
-                                </div>
-                            </div>
-
-                            <div>
-                                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                                    Password
-                                </label>
-                                <div className="mt-2">
-                                    <input
-                                        id="password"
-                                        name="password"
-                                        type="password"
-                                        value={formData.password}
-                                        onChange={(e) => handleChange('password', e.target.value)}
-                                        autoComplete="current-password"
-                                        required
-                                        className="form-input block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-beige sm:text-sm sm:leading-6"
-                                    />
-                                    <div className="text-red-500 text-sm mt-2">{errors.password}</div>
-
-                                </div>
-                            </div>
-
-                            <div className="flex items-center justify-between">
-                                <div className="flex items-center">
-                                    <input
-                                        id="remember-me"
-                                        name="remember-me"
-                                        type="checkbox"
-                                        className="h-4 w-4  form-radio rounded border-gray-300 text-beige focus:ring-beige"
-                                    />
-                                    <label htmlFor="remember-me" className="ml-3 block text-sm leading-6 text-gray-900">
-                                        Remember me
-                                    </label>
-                                </div>
-
-                                <div className="text-sm leading-6">
-                                    <a href="#" className="font-semibold text-beige hover:text-beige">
-                                        Forgot password?
-                                    </a>
-                                </div>
-                            </div>
-
-                            <div>
-                                <button
-                                    disabled={loading}
-                                    type="submit"
-                                    className={`flex w-full justify-center rounded-md bg-beige px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-beige focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-beige  ${loading ? 'opacity-50 cursor-not-allowed' : ''}`}
-                                >
-
-                                    {loading ? 'Loading...' : 'Sign in'}
-                                </button>
-                            </div>
-                        </form>
-
-                        <div>
-                        </div>
+                        <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                            Sign in to your account
+                        </h2>
                     </div>
 
 
+
+                    <form className="space-y-6" noValidate autoComplete="off" onSubmit={handleSubmit} >
+                        <div>
+                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                                Email address
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    id="email"
+                                    name="email"
+                                    type="email"
+                                    value={formData.email}
+                                    onChange={(e) => handleChange('email', e.target.value)}
+                                    autoComplete="email"
+                                    required
+                                    className="form-input block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-beige sm:text-sm sm:leading-6"
+                                />
+                                <div className="text-red-500 text-sm mt-2">{errors.email}</div>
+
+                            </div>
+                        </div>
+
+                        <div>
+                            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                                Password
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    value={formData.password}
+                                    onChange={(e) => handleChange('password', e.target.value)}
+                                    autoComplete="current-password"
+                                    required
+                                    className="form-input block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-beige sm:text-sm sm:leading-6"
+                                />
+                                <div className="text-red-500 text-sm mt-2">{errors.password}</div>
+
+                            </div>
+                        </div>
+
+                        <div className="flex items-center justify-between">
+                            <div className="flex items-center">
+                                <input
+                                    id="remember-me"
+                                    name="remember-me"
+                                    type="checkbox"
+                                    className="h-4 w-4  form-radio rounded border-gray-300 text-beige focus:ring-beige"
+                                />
+                                <label htmlFor="remember-me" className="ml-3 block text-sm leading-6 text-gray-900">
+                                    Remember me
+                                </label>
+                            </div>
+
+                            <div className="text-sm leading-6">
+                                <a href="#" className="font-semibold text-beige hover:text-beige">
+                                    Forgot password?
+                                </a>
+                            </div>
+                        </div>
+
+                        <div>
+                            <button
+                                disabled={loading}
+                                type="submit"
+                                className={`flex w-full justify-center rounded-md bg-beige px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-beige focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-beige  ${loading ? 'opacity-50 cursor-not-allowed' : ''}`}
+                            >
+
+                                {loading ? 'Loading...' : 'Sign in'}
+                            </button>
+                        </div>
+                    </form>
+
+                    <div>
+                    </div>
                 </div>
+
+
             </div>
+        </div>
 
     )
 }
